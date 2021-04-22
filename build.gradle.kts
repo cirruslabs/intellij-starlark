c4753ba1c483b44c43b89eeda97c7c202585af63
fedor <fedor@cirruslabs.org>
2021-04-22 16:41:20 -0400

    Java 1.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,20 @@
 import org.jetbrains.intellij.tasks.PatchPluginXmlTask
 import org.jetbrains.intellij.tasks.PublishTask
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
   kotlin("jvm") version "1.4.32"
   id("org.jetbrains.intellij") version "0.7.2"
+}
+
+tasks.withType<JavaCompile> {
+  sourceCompatibility = "1.8"
+  targetCompatibility = "1.8"
+}
+listOf("compileKotlin", "compileTestKotlin").forEach {
+  tasks.getByName<KotlinCompile>(it) {
+    kotlinOptions.jvmTarget = "1.8"
+  }
 }
 
 repositories {
