389fb5de6d184aff90be6b62a366c1bade4a56e2
fedor <fedor@cirruslabs.org>
2021-04-22 16:31:06 -0400

    Removed non-existing test

--- src/test/kotlin/org/cirruslabs/intellij/starlark/StarlarkCompletionTest.kt
+++ src/test/kotlin/org/cirruslabs/intellij/starlark/StarlarkCompletionTest.kt
@@ -5,30 +5,26 @@ import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase
 
 
 class StarlarkCompletionTest : LightJavaCodeInsightFixtureTestCase() {
-  override fun getTestDataPath(): String {
-    return "src/test/testData/completion"
-  }
+    override fun getTestDataPath(): String {
+        return "src/test/testData/completion"
+    }
 
-  private fun assertExpectedLookups(filePath: String, vararg expected: String) {
-    myFixture.configureByFiles(filePath)
-    myFixture.complete(CompletionType.BASIC, 1)
-    val lookupElement = myFixture.lookupElementStrings ?: emptyList()
-    assertContainsElements(lookupElement, *expected)
-  }
+    private fun assertExpectedLookups(filePath: String, vararg expected: String) {
+        myFixture.configureByFiles(filePath)
+        myFixture.complete(CompletionType.BASIC, 1)
+        val lookupElement = myFixture.lookupElementStrings ?: emptyList()
+        assertContainsElements(lookupElement, *expected)
+    }
 
-  fun testBuiltin() {
-    assertExpectedLookups("builtin.star", "print", "load")
-  }
+    fun testBuiltin() {
+        assertExpectedLookups("builtin.star", "print", "load")
+    }
 
-  fun testLoadCirrusEnv() {
-    assertExpectedLookups("load_cirrus_env.star", "get")
-  }
+    fun testLoadCirrusFS() {
+        assertExpectedLookups("load_cirrus_fs.star", "exists")
+    }
 
-  fun testLoadCirrusFS() {
-    assertExpectedLookups("load_cirrus_fs.star", "exists")
-  }
-
-  fun testLoadCirrusJson() {
-    assertExpectedLookups("load_cirrus_json.star", "json", "json_alias")
-  }
+    fun testLoadCirrusJson() {
+        assertExpectedLookups("load_cirrus_json.star", "json", "json_alias")
+    }
 }
