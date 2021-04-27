f8e9626e9da057d5e73264ea0fa4a8feec55c8fc
fedor <fedor@cirruslabs.org>
2021-04-27 14:39:11 -0400

    Don't show interpreter inspection

---
+++ src/main/kotlin/org/cirruslabs/intellij/starlark/inspections/StarlarkPyInspectionExtension.kt
@@ -0,0 +1,11 @@
+package org.cirruslabs.intellij.starlark.inspections
+
+import com.jetbrains.python.inspections.PyInspectionExtension
+import com.jetbrains.python.psi.PyFile
+import org.cirruslabs.intellij.starlark.StarlarkLanguage
+
+class StarlarkPyInspectionExtension : PyInspectionExtension() {
+    override fun ignoreInterpreterWarnings(file: PyFile): Boolean {
+        return file.language is StarlarkLanguage
+    }
+}
