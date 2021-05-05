868b1dbb4c6875a77bbca6467d667dc0ec713922
fedor <fedor@cirruslabs.org>
2021-05-05 07:35:21 -0400

    Download modules

--- src/main/kotlin/org/cirruslabs/intellij/starlark/modules/CirrusModuleManager.kt
+++ src/main/kotlin/org/cirruslabs/intellij/starlark/modules/CirrusModuleManager.kt
@@ -1,5 +1,6 @@
 package org.cirruslabs.intellij.starlark.modules
 
+import com.intellij.openapi.application.PathManager
 import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiFile
@@ -7,28 +8,40 @@ import com.intellij.psi.PsiManager
 import com.intellij.util.ResourceUtil
 import com.intellij.util.indexing.IndexableSetContributor
 import com.jetbrains.python.psi.PyFile
-
+import java.nio.file.Path
+import java.nio.file.Paths
+import kotlin.io.path.Path
 
 class CirrusModuleManager : IndexableSetContributor() {
-  companion object {
-    private val CIRRUS_MODULE by lazy {
-      val url = ResourceUtil.getResource(javaClass.classLoader, "modules", "cirrus.star")
-      VfsUtil.findFileByURL(url)
+    companion object {
+        private val CIRRUS_MODULE by lazy {
+            val url = ResourceUtil.getResource(javaClass.classLoader, "modules", "cirrus.star")
+            VfsUtil.findFileByURL(url)
+        }
+
+        fun findModule(module: String): VirtualFile? {
+            if (module == "cirrus") return CIRRUS_MODULE
+            return try {
+                val locator = ModuleLocator.parse(module)
+                VfsUtil.findFile(modulePath(locator), true)?.findChild(locator.path ?: "lib.star")
+            } catch (ex: IllegalStateException) {
+                null
+            }
+        }
+
+        fun resolveModuleFile(originalFile: PsiFile, module: String): PyFile? {
+            return findModule(module)?.let {
+                PsiManager.getInstance(originalFile.project).findFile(it) as? PyFile
+            }
+        }
+
+        private val globalCacheLocation: Path
+            get() = Paths.get(PathManager.getSystemPath(), "extStarlarkModules")
+
+        fun modulePath(module: ModuleLocator): Path = globalCacheLocation.resolve(module.org).resolve(module.repo)
     }
 
-    fun findModule(module: String): VirtualFile? {
-      if (module == "cirrus") return CIRRUS_MODULE
-      return null
+    override fun getAdditionalRootsToIndex(): Set<VirtualFile> {
+        return setOfNotNull(CIRRUS_MODULE?.parent, VfsUtil.findFile(globalCacheLocation, true))
     }
-
-    fun resolveModuleFile(originalFile: PsiFile, module: String): PyFile? {
-      return findModule(module)?.let {
-        PsiManager.getInstance(originalFile.project).findFile(it) as? PyFile
-      }
-    }
-  }
-
-  override fun getAdditionalRootsToIndex(): Set<VirtualFile> {
-    return setOfNotNull(CIRRUS_MODULE?.parent)
-  }
 }
--- src/main/kotlin/org/cirruslabs/intellij/starlark/modules/DownloadGitHubModuleQuickFix.kt
+++ src/main/kotlin/org/cirruslabs/intellij/starlark/modules/DownloadGitHubModuleQuickFix.kt
@@ -1,15 +1,47 @@
 package org.cirruslabs.intellij.starlark.modules
 
+import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer
 import com.intellij.codeInspection.LocalQuickFix
 import com.intellij.codeInspection.ProblemDescriptor
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.progress.Task.Backgroundable
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.io.FileUtil
+import com.intellij.platform.templates.github.DownloadUtil
+import com.intellij.platform.templates.github.ZipUtil
+import org.cirruslabs.intellij.starlark.StarlarkBundle
+import java.io.BufferedInputStream
+import java.io.File
+import java.io.FileInputStream
+import java.util.zip.ZipInputStream
 
 class DownloadGitHubModuleQuickFix(val module: ModuleLocator) : LocalQuickFix {
-  constructor(module: String): this(ModuleLocator.parse(module))
+  constructor(module: String) : this(ModuleLocator.parse(module))
 
   override fun getFamilyName(): String =
     "Download ${module.org}/${module.repo} module"
 
   override fun applyFix(project: Project, descriptor: ProblemDescriptor) {
+    val containingFile = descriptor.psiElement.containingFile
+    ProgressManager.getInstance().run(object : Backgroundable(project, StarlarkBundle.getMessage("starlark.intention.fetch.progress.fetching.module")) {
+      override fun run(indicator: ProgressIndicator) {
+        val moduleURL = "https://github.com/${module.org}/${module.repo}/archive/${module.revision ?: "master"}.zip"
+        val tmpFile = File.createTempFile("download", "module")
+        tmpFile.deleteOnExit()
+        try {
+          val modulePath = CirrusModuleManager.modulePath(module)
+          DownloadUtil.downloadAtomically(indicator, moduleURL, tmpFile)
+          FileUtil.delete(modulePath)
+          ZipUtil.unzip(indicator, modulePath, ZipInputStream(BufferedInputStream(FileInputStream(tmpFile))), null, null, true)
+        } finally {
+          tmpFile.delete()
+        }
+        ApplicationManager.getApplication().invokeLater {
+          DaemonCodeAnalyzer.getInstance(project).restart(containingFile)
+        }
+      }
+    })
   }
 }
