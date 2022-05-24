9ab724f0690d962066ecad2b4497e401c51e07ac
fedor <fedor.korotkov@gmail.com>
2022-05-24 09:46:49 -0400

    Don't use deprecated API

--- src/main/kotlin/org/cirruslabs/intellij/starlark/modules/UnresolvedModuleReferenceQuickFixProvider.kt
+++ src/main/kotlin/org/cirruslabs/intellij/starlark/modules/UnresolvedModuleReferenceQuickFixProvider.kt
@@ -6,11 +6,11 @@ import com.intellij.util.Consumer
 import com.jetbrains.python.inspections.PyUnresolvedReferenceQuickFixProvider
 
 class UnresolvedModuleReferenceQuickFixProvider : PyUnresolvedReferenceQuickFixProvider {
-  override fun registerQuickFixes(ref: PsiReference, consumer: Consumer<LocalQuickFix>) {
+  override fun registerQuickFixes(ref: PsiReference, existing: MutableList<LocalQuickFix>) {
     if (ref is ModuleReference && ref.module.startsWith("github.com/")) {
       try {
-        consumer.consume(DownloadGitHubModuleQuickFix(ref.module))
-      } catch (ex: IllegalStateException) {
+        existing.add(DownloadGitHubModuleQuickFix(ref.module))
+      } catch (_: IllegalStateException) {
       }
     }
   }
